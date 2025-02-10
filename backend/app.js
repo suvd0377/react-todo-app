140c58a05e6fd2bb6e14d00d14119e814773962f
suvd0377 <suvd037@icloud.com>
2025-02-10 11:44:43 +0900

    000

---
+++ backend/app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const PORT = 8080;
+const { sequelize } = require('./models');
+const indexRouter = require('./routes');
+const serverPerfix = '/api-server';
+const cors = require('cors');
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cors());
+// /api-server
+app.use(serverPerfix, indexRouter);
+
+sequelize
+  .sync({ force: false })
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`http://localhost:${PORT}`);
+    });
+  })
+  .catch(err => {
+    console.log(err);
+    console.log('database sync 오류!');
+  });
---
+++ backend/routes/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const router = express.Router();
+const controller = require('../controller/Cmain');
+
+// GET /api-server
+router.get('/', controller.getIndex);
+
+// GET /api-server/user
+router.get('/user', controller.getUser);
+
+///////////////
+// GET /api-server/todos
+router.get('/todos', controller.getTodos);
+
+// POST /api-server/todo
+router.post('/todo', controller.addTodo);
+
+// PATCH /api-server/todo/:todoId
+router.patch('/todo/:todoId', controller.patchDoneState);
+
+// DELETE /api-server/todo/:todoId
+router.delete('/todo/:todoId', controller.deleteTodo);
+
+// PATCH /api-server/content
+router.patch('/content');
+
+module.exports = router;
